0b4cccdd0ad56bdfada37668c0eefec0f4f52ac2
Kirill Guryanov <korako2@mail.ru>
2023-10-25 18:59:26 +0300

    feat: insert to 2 tables

--- insert.sql
+++ insert.sql
@@ -12,3 +12,19 @@ INSERT INTO vehicle_movement_history (vehicle_id, date, latitude, longitude, mil
 VALUES (1, '2019-01-01 00:00:00', 1.0, 1.0, 1000),
        (2, '2019-01-01 00:00:01', 1.0, 1.0, 1400),
        (1, '2019-01-01 00:00:03', 1.0, 1.0, 1100);
+
+TRUNCATE TABLE address CASCADE;
+ALTER SEQUENCE address_id_seq RESTART WITH 1;
+INSERT INTO address (address_id, country, city, street, building, corpus)
+VALUES (1, 'Россия', 'Санкт-Петербург', 'Кронверский проспект', '49', NULL),
+       (2, 'Россия', 'Москва', 'Новая Басманная', 4, 1),
+       (3, 'Беларусь', 'Минск', 'Кирова', 8, 7),
+       (4, 'Беларусь', 'Солигорск', 'Козлова', 35, NULL);
+
+
+TRUNCATE TABLE storage_point CASCADE;
+INSERT INTO storage_point(address_id, longitude, latitude)
+VALUES (1, 59.956363, 30.310029),
+       (2, 55.769048, 37.653765),
+       (3, 53.895058, 27.561382),
+       (4, 52.784080, 27.543973);
