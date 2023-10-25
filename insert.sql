7791f57d4418d3fdae3f30183a5ea7cf58966cce
Kirill Guryanov <korako2@mail.ru>
2023-10-25 21:45:18 +0300

    feat: add inserts

--- insert.sql
+++ insert.sql
@@ -14,7 +14,7 @@ VALUES (1, '2019-01-01 00:00:00', 1.0, 1.0, 1000),
        (1, '2019-01-01 00:00:03', 1.0, 1.0, 1100);
 
 TRUNCATE TABLE address CASCADE;
-ALTER SEQUENCE address_id_seq RESTART WITH 1;
+ALTER SEQUENCE address_address_id_seq RESTART WITH 1;
 INSERT INTO address (address_id, country, city, street, building, corpus)
 VALUES (1, 'Россия', 'Санкт-Петербург', 'Кронверский проспект', '49', NULL),
        (2, 'Россия', 'Москва', 'Новая Басманная', 4, 1),
@@ -28,3 +28,28 @@ VALUES (1, 59.956363, 30.310029),
        (2, 55.769048, 37.653765),
        (3, 53.895058, 27.561382),
        (4, 52.784080, 27.543973);
+
+
+TRUNCATE TABLE person CASCADE;
+ALTER SEQUENCE person_person_id_seq RESTART WITH 1;
+INSERT INTO person (person_id, first_name, last_name, middle_name, gender, passport)
+VALUES
+  (1, 'Иван', 'Иванов', 'Иванович', 'M', '1234567890'),
+  (2, 'Мария', 'Петрова', 'Александровна', 'F', '0987654321'),
+  (3, 'Алексей', 'Сидоров', NULL, 'M', '5432109876'),
+  (4, 'Екатерина', 'Смирнова', 'Андреевна', 'F', '1357924680');
+
+TRUNCATE TABLE customer CASCADE;
+INSERT INTO customer (person_id, organization)
+VALUES
+  (1, 'ООО Рога и Копыта'),
+  (2, 'ИП Иванов И.И.'),
+  (3, 'ЗАО Петров и Партнеры'),
+  (4, NULL);
+
+TRUNCATE TABLE orders CASCADE;
+INSERT INTO orders (customer_id, distance, price, order_date, vehicle_id)
+VALUES
+  (1, 800, 30000, '2023-10-25', 1),
+  (2, 700, 28000, '2023-10-26', 2),
+
