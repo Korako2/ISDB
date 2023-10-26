90fca573fc027ec5e07bc5398016a55bffadaed1
Moleus <fafufuburr@gmail.com>
2023-10-26 10:14:42 +0300

    feat: vehicle/cargo type trigger & fuel expenses

--- triggers.sql
+++ triggers.sql
@@ -26,34 +26,39 @@ CREATE TRIGGER check_speed_trigger
   BEFORE INSERT OR UPDATE ON vehicle_movement_history
   FOR EACH ROW EXECUTE PROCEDURE check_speed();
 
-
--- Creating the "VEHICLE" table
-CREATE TABLE IF NOT EXISTS VEHICLE (
-  VEHICLE_ID serial PRIMARY KEY,
-  PLATE_NUMBER varchar(9) NOT NULL CHECK (
-    PLATE_NUMBER ~ '^[А-Я]{1}\d{3}[А-Я]{2}\d{2}$' OR
-    PLATE_NUMBER ~ '^[А-Я]{1}\d{3}[А-Я]{2}\d{3}$'
-  ),
-  MODEL varchar(50) NOT NULL,
-  MANUFACTURE_YEAR date NOT NULL,
-  LENGTH float NOT NULL,
-  WIDTH float NOT NULL,
-  HEIGHT float NOT NULL,
-  LOAD_CAPACITY float NOT NULL,
-  BODY_TYPE BODY_TYPE
-);
-
--- Creating the "BODY TYPE" enumeration
-CREATE TYPE BODY_TYPE AS ENUM (
-  'OPEN',
-  'CLOSED'
-);
-
-CREATE TYPE CARGO_TYPE AS ENUM (
-  'BULK',
-  'TIPPER',
-  'PALLETIZED'
-);
-
 -- Подобранный автомобиль должен соответствовать типу груза. Насыпной, навалочный -- открытый. Тарный -- закрытый.
+CREATE OR REPLACE FUNCTION check_vehicle_type RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.CARGO_TYPE = 'BULK' OR NEW.CARGO_TYPE = 'TIPPER' THEN
+    IF NEW.BODY_TYPE != 'OPEN' THEN
+      RAISE EXCEPTION 'Vehicle type must be OPEN';
+    END IF;
+  END IF;
+  IF NEW.CARGO_TYPE = 'PALLETIZED' THEN
+    IF NEW.BODY_TYPE != 'CLOSED' THEN
+      RAISE EXCEPTION 'Vehicle type must be CLOSED';
+    END IF;
+  END IF;
+  RETURN NEW;
+END;
+END;
+$$ LANGUAGE plpgsql;
 
+-- расходы должны сопоставляться с пробегом автомобиля
+-- Fuel price is 60 RUB per liter. Fuel consumption is 10 liters per 100 km. So 6 RUB per km is the maximum allowed value
+CREATE OR REPLACE FUNCTION check_fuel_expenses() RETURNS TRIGGER AS $$
+DECLARE
+  prev_pay_date timestamp;
+  prev_mileage float;
+  current_mileage float;
+BEGIN
+  -- select record from movement history nearest to prev_pay_date
+  prev_pay_date = (SELECT DATE FROM FUEL_EXPENSES WHERE FUEL_CARD_NUMBER = NEW.FUEL_CARD_NUMBER ORDER BY DATE DESC LIMIT 1);
+  prev_mileage = (SELECT MILEAGE FROM vehicle_movement_history WHERE VEHICLE_ID = NEW.VEHICLE_ID AND DATE <= prev_pay_date ORDER BY DATE DESC LIMIT 1);
+  current_mileage = (SELECT MILEAGE FROM vehicle_movement_history WHERE VEHICLE_ID = NEW.VEHICLE_ID AND DATE >= prev_pay_date ORDER BY DATE DESC LIMIT 1);
+  IF (current_mileage - prev_mileage) * 6 < NEW.AMOUNT THEN
+    RAISE EXCEPTION 'Fuel expenses are too high';
+  END IF;
+
+  RETURN NEW;
+END;
