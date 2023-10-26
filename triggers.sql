969485dad1ffdc90986b5e36dc2a498779358b94
Pavel Soloviev <66182149+Moleus@users.noreply.github.com>
2023-10-26 12:00:22 +0300

    feat(triggers): vehicle/cargo type trigger & fuel expenses & speed (#4)
    * add triggers
    * feat: vehicle triggers
    * feat: vehicle/cargo type trigger & fuel expenses

--- triggers.sql
+++ triggers.sql
@@ -1,15 +1,4 @@
 -- triggers & functions
--- Ограничение по макс. изменению расстояния. Скорость авто не может быть больше 170км/ч
-
--- Creating the "VEHICLE MOVEMENT HISTORY" table
--- CREATE TABLE IF NOT EXISTS VEHICLE_MOVEMENT_HISTORY (
---   VEHICLE_ID int REFERENCES VEHICLE (VEHICLE_ID),
---   DATE timestamp,
---   LATITUDE float NOT NULL,
---   LONGITUDE float NOT NULL,
---   MILEAGE float NOT NULL,
---   PRIMARY KEY (VEHICLE_ID, DATE)
--- );
 
 -- calculate difference between new and last distance for current VEHICLE_ID and divide by date difference
 CREATE OR REPLACE FUNCTION check_speed() RETURNS TRIGGER AS $$
@@ -37,9 +26,41 @@ CREATE TRIGGER check_speed_trigger
   BEFORE INSERT OR UPDATE ON vehicle_movement_history
   FOR EACH ROW EXECUTE PROCEDURE check_speed();
 
+-- Подобранный автомобиль должен соответствовать типу груза. Насыпной, навалочный -- открытый. Тарный -- закрытый.
+CREATE OR REPLACE FUNCTION check_vehicle_type() RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.CARGO_TYPE = 'BULK' OR NEW.CARGO_TYPE = 'TIPPER' THEN
+    IF NEW.BODY_TYPE != 'OPEN' THEN
+      RAISE EXCEPTION 'Vehicle type must be OPEN';
+    END IF;
+  END IF;
+  IF NEW.CARGO_TYPE = 'PALLETIZED' THEN
+    IF NEW.BODY_TYPE != 'CLOSED' THEN
+      RAISE EXCEPTION 'Vehicle type must be CLOSED';
+    END IF;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-
-
+-- расходы должны сопоставляться с пробегом автомобиля
+-- Fuel price is 60 RUB per liter. Fuel consumption is 10 liters per 100 km. So 6 RUB per km is the maximum allowed value
+CREATE OR REPLACE FUNCTION check_fuel_expenses() RETURNS TRIGGER AS $$
+DECLARE
+  prev_pay_date timestamp;
+  prev_mileage float;
+  current_mileage float;
+BEGIN
+  -- select record from movement history nearest to prev_pay_date
+  prev_pay_date = (SELECT DATE FROM FUEL_EXPENSES WHERE FUEL_CARD_NUMBER = NEW.FUEL_CARD_NUMBER ORDER BY DATE DESC LIMIT 1);
+  prev_mileage = (SELECT MILEAGE FROM vehicle_movement_history WHERE VEHICLE_ID = NEW.VEHICLE_ID AND DATE <= prev_pay_date ORDER BY DATE DESC LIMIT 1);
+  current_mileage = (SELECT MILEAGE FROM vehicle_movement_history WHERE VEHICLE_ID = NEW.VEHICLE_ID AND DATE >= prev_pay_date ORDER BY DATE DESC LIMIT 1);
+  IF (current_mileage - prev_mileage) * 6 < NEW.AMOUNT THEN
+    RAISE EXCEPTION 'Fuel expenses are too high';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION check_cargo_size()
@@ -71,13 +92,10 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER cargo_check_size
+CREATE TRIGGER cargo_check_size
 BEFORE INSERT ON cargo
 FOR EACH ROW
-EXECUTE FUNCTION check_cargo_size();
-
-
-
+EXECUTE PROCEDURE check_cargo_size();
 
 
 CREATE OR REPLACE FUNCTION check_country_match()
@@ -120,7 +138,6 @@ FOR EACH ROW
 EXECUTE FUNCTION check_country_match();
 
 
-
 CREATE OR REPLACE FUNCTION check_order_status_sequence()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -151,7 +168,6 @@ CREATE TRIGGER order_status_sequence_check
 BEFORE INSERT ON order_statuses
 FOR EACH ROW
 EXECUTE FUNCTION check_order_status_sequence();
-
 
 
 CREATE OR REPLACE FUNCTION check_order_status_time()
