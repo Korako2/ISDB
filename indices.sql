1a46a3c47a1d22ff4035ebb7c8ed6397bd346ba8
Moleus <fafufuburr@gmail.com>
2023-10-27 11:37:31 +0300

    fix: remove invalid index

--- indices.sql
+++ indices.sql
@@ -1,7 +1,3 @@
--- мы делаем селекты по дате при проверках трат по карточке. По-дефолту - B-Tree
-CREATE INDEX vehicle_movement_history_date_idx ON vehicle_movement_history (date);
-
--- Индексы на долготу/широту авто
 CREATE INDEX vehicle_movement_history_location_idx ON vehicle_movement_history USING btree (longitude, latitude);
 
 CREATE INDEX driver_status_history_status_idx ON driver_status_history USING hash (status);
