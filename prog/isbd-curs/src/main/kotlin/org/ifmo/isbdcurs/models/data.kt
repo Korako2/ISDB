a28ad21285581d710f7c58ee6af7b33814647938
Moleus <fafufuburr@gmail.com>
2023-11-02 22:32:44 +0300

    feat: use Spring Data JDBC instead of JPA

--- prog/isbd-curs/src/main/kotlin/org/ifmo/isbdcurs/models/data.kt
+++ prog/isbd-curs/src/main/kotlin/org/ifmo/isbdcurs/models/data.kt
@@ -1,13 +1,7 @@
 package org.ifmo.isbdcurs.models
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.validation.constraints.DecimalMax
-import jakarta.validation.constraints.Max
-import org.hibernate.annotations.Check
-import org.hibernate.validator.constraints.Range
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 import java.sql.Timestamp
 import java.time.Instant
 import java.util.*
@@ -32,169 +26,154 @@ enum class ContactInfoType {
     PHONE_NUMBER, TELEGRAM, EMAIL,
 }
 
-@Entity
-open class Person(
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) open val id: Long? = null,
-    open val firstName: String,
-    open val lastName: String,
-    open val middleName: String?,
-    @Check(constraints = "gender IN ('M', 'F')") open val gender: Char,
-    @Check(constraints = "date_of_birth >= '1910-01-01'") open val dateOfBirth: Date,
+@Table
+data class Person(
+    @Id val id: Long? = null,
+    val firstName: String,
+    val lastName: String,
+    val middleName: String?,
 )
 
-@Entity
-open class ContactInfo(
-    @Id open val personId: Long,
-    @Id open val contactType: ContactInfoType,
-    open val value: String,
+@Table
+data class ContactInfo(
+    @Id val personId: Long,
+    @Id val contactType: ContactInfoType,
+    val value: String,
 )
 
-@Entity
-open class Driver(
-    @Id open val id: Long? = null,
-    open val personId: Long,
-    @Check(constraints = "passport ~ '^[0-9]{10}$'") open val passport: String,
-    open val bankCardNumber: String,
+@Table
+data class Driver(
+    @Id val id: Long? = null,
+    val personId: Long,
+    val bankCardNumber: String,
 )
 
-@Entity
-open class Customer(
-    @Id open val id: Long? = null,
-    open val personId: Long,
-    open val organization: String?,
+@Table
+data class Customer(
+    @Id val id: Long? = null,
+    val personId: Long,
+    val organization: String?,
 )
 
-@Entity
-open class DriverStatusHistory(
-    @Id open val driverId: Long,
-    @Id open val date: Instant,
-    open val status: DriverStatus,
+@Table
+data class DriverStatusHistory(
+    @Id val driverId: Long,
+    @Id val date: Instant,
+    val status: DriverStatus,
 )
 
-@Entity
-open class TariffRate(
-    @Id open val driverId: Long,
-    open val dailyRate: Int,
-    open val ratePerKm: Int,
+@Table
+data class TariffRate(
+    @Id val driverId: Long,
+    val dailyRate: Int,
+    val ratePerKm: Int,
 )
 
-@Entity
-@Check(constraints = "issueDate < expirationDate")
-open class DriverLicense(
-    @Id open val driverId: Long,
-    open val issueDate: Instant,
-    open val expirationDate: Instant,
-    open val licenseNumber: Int,
+@Table
+data class DriverLicense(
+    @Id val driverId: Long,
+    val issueDate: Instant,
+    val expirationDate: Instant,
+    val licenseNumber: Int,
 )
 
-@Entity
-open class Vehicle(
-    @Id open val id: Long? = null,
-    @Check(constraints = "plateNumber ~ '^[А-Я]{1}\\d{3}[А-Я]{2}\\d{2}$' OR plateNumber ~ '^[А-Я]{1}\\d{3}[А-Я]{2}\\d{3}$'")
-    open val plateNumber: String,
-    open val model: String,
-    open val manufactureYear: Instant,
-    open val length: Float,
-    open val width: Float,
-    open val height: Float,
-    open val loadCapacity: Float,
-    open val bodyType: BodyType,
+@Table
+data class Vehicle(
+    @Id val id: Long? = null,
+    val plateNumber: String,
+    val model: String,
+    val manufactureYear: Instant,
+    val length: Float,
+    val width: Float,
+    val height: Float,
+    val loadCapacity: Float,
+    val bodyType: BodyType,
 )
 
-@Entity
-@Check(constraints = "ownershipStartDate <= ownershipEndDate")
-open class VehicleOwnership(
-    @Id open val vehicleId: Long,
-    @Id open val driverId: Long,
-    open val ownershipStartDate: Instant,
-    open val ownershipEndDate: Instant,
+@Table
+data class VehicleOwnership(
+    @Id val vehicleId: Long,
+    @Id val driverId: Long,
+    val ownershipStartDate: Instant,
+    val ownershipEndDate: Instant,
 )
 
-@Entity
-open class VehicleMovementHistory(
-    @Id open val vehicleId: Long,
-    @Id open val date: Date,
-    open val latitude: Float,
-    open val longitude: Float,
-    open val mileage: Float,
+@Table
+data class VehicleMovementHistory(
+    @Id val vehicleId: Long,
+    @Id val date: Date,
+    val latitude: Float,
+    val longitude: Float,
+    val mileage: Float,
 )
 
-@Entity
-open class Order(
-    @Id open val id: Long? = null,
-    open val customerId: Long,
-    open val distance: Float,
-    open val price: Float,
-    open val orderDate: Date,
-    open val vehicleId: Long?,
+@Table
+data class Order(
+    @Id val id: Long? = null,
+    val customerId: Long,
+    val distance: Float,
+    val price: Float,
+    val orderDate: Date,
+    val vehicleId: Long?,
 )
 
-@Entity
-open class OrderStatuses(
-    @Id open val orderId: Long,
-    @Id open val dateTime: Timestamp,
-    open val status: OrderStatus,
+@Table
+data class OrderStatuses(
+    @Id val orderId: Long,
+    @Id val dateTime: Timestamp,
+    val status: OrderStatus,
 )
 
-@Entity
-open class Cargo(
-    @Id open val id: Long? = null,
-    @Max(25000)
-    open val weight: Float,
-    @DecimalMax("2.5")
-    open val width: Float,
-    @Max(4)
-    open val height: Float,
-    @Max(15)
-    open val length: Float,
-    open val orderId: Long,
-    open val cargoType: CargoType,
+@Table
+data class Cargo(
+    @Id val id: Long? = null,
+    val weight: Float,
+    val width: Float,
+    val height: Float,
+    val length: Float,
+    val orderId: Long,
+    val cargoType: CargoType,
 )
 
-@Entity
-open class Address(
-    @Id open val id: Long? = null,
-    open val country: String,
-    open val city: String,
-    open val street: String,
-    open val building: Int,
-    open val corpus: Int?,
+@Table
+data class Address(
+    @Id val id: Long? = null,
+    val country: String,
+    val city: String,
+    val street: String,
+    val building: Int,
+    val corpus: Int?,
 )
 
-@Entity
-open class StoragePoint(
-    @Id open val addressId: Long,
-    @Range(min=-180, max=180)
-    open val longitude: Float,
-    @Range(min=-90, max=90)
-    open val latitude: Float,
+@Table
+data class StoragePoint(
+    @Id val addressId: Long,
+    val longitude: Float,
+    val latitude: Float,
 )
 
-@Entity
-@Check(constraints = "departurePoint <> deliveryPoint")
-open class LoadingUnloadingAgreement(
-    @Id open val orderId: Long,
-    open val driverId: Long,
-    open val departurePoint: Long,
-    open val deliveryPoint: Long,
-    open val senderId: Long,
-    open val receiverId: Long,
-    @Check(constraints = "unloadingTime >= '00:01'::time AND unloadingTime < '24:00'::time")
-    open val unloadingTime: Timestamp,
-    @Check(constraints = "loadingTime >= '00:01'::time AND loadingTime < '24:00'::time")
-    open val loadingTime: Timestamp,
+@Table
+data class LoadingUnloadingAgreement(
+    @Id val orderId: Long,
+    val driverId: Long,
+    val departurePoint: Long,
+    val deliveryPoint: Long,
+    val senderId: Long,
+    val receiverId: Long,
+    val unloadingTime: Timestamp,
+    val loadingTime: Timestamp,
 )
 
-@Entity
-open class FuelCardsForDrivers(
-    @Id open val driverId: Long,
-    @Id open val fuelCardNumber: String,
-    open val fuelStationName: String?,
+@Table
+data class FuelCardsForDrivers(
+    @Id val driverId: Long,
+    @Id val fuelCardNumber: String,
+    val fuelStationName: String?,
 )
 
-@Entity
-open class FuelExpenses(
-    @Id open val fuelCardNumberId: Long,
-    open val date: Date,
-    open val amount: Double,
+@Table
+data class FuelExpenses(
+    @Id val fuelCardNumberId: Long,
+    val date: Date,
+    val amount: Double,
 )
