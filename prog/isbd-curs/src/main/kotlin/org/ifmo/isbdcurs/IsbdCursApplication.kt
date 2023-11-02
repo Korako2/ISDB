7c2e44107a619c933e9fc0f347d0fecc0adc535c
Moleus <fafufuburr@gmail.com>
2023-11-02 18:41:58 +0300

    feat: init jpa project

---
+++ prog/isbd-curs/src/main/kotlin/org/ifmo/isbdcurs/IsbdCursApplication.kt
@@ -0,0 +1,11 @@
+package org.ifmo.isbdcurs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IsbdCursApplication
+
+fun main(args: Array<String>) {
+    runApplication<IsbdCursApplication>(*args)
+}
---
+++ prog/isbd-curs/src/test/kotlin/org/ifmo/isbdcurs/IsbdCursApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ifmo.isbdcurs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IsbdCursApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
