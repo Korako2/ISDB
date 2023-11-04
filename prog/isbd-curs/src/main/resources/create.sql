a66f8fea3f5ca485877e9f5ebea9271903159166
Moleus <fafufuburr@gmail.com>
2023-11-04 17:59:03 +0300

    fix(sql): don't cascade drop ENUM cause it removes column from tables

--- prog/isbd-curs/src/main/resources/create.sql
+++ prog/isbd-curs/src/main/resources/create.sql
@@ -1,4 +1,3 @@
-DROP TYPE IF EXISTS driver_status CASCADE;
 CREATE TYPE driver_status AS ENUM (
   'ACCEPTED ORDER',
   'OFF DUTY',
@@ -10,20 +9,17 @@ CREATE TYPE driver_status AS ENUM (
   'COMPLETED ORDER'
 );
 
-DROP TYPE IF EXISTS body_type CASCADE;
 CREATE TYPE body_type AS ENUM (
   'OPEN',
   'CLOSED'
 );
 
-DROP TYPE IF EXISTS cargo_type CASCADE;
 CREATE TYPE cargo_type AS ENUM (
   'BULK',
   'TIPPER',
   'PALLETIZED'
 );
 
-DROP TYPE IF EXISTS order_status CASCADE;
 CREATE TYPE order_status AS ENUM (
   'ACCEPTED',
   'IN PROGRESS',
@@ -35,7 +31,6 @@ CREATE TYPE order_status AS ENUM (
   'COMPLETED'
 );
 
-DROP TYPE IF EXISTS contact_info_type CASCADE;
 CREATE TYPE contact_info_type AS ENUM (
   'PHONE NUMBER',
   'TELEGRAM',
@@ -104,7 +99,7 @@ CREATE TABLE IF NOT EXISTS vehicle (
   width float NOT NULL,
   height float NOT NULL,
   load_capacity float NOT NULL,
-  body_type body_type
+  body_type body_type NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS vehicle_ownership (
