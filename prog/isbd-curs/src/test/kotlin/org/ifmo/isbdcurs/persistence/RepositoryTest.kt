f33d515805bea6f194594190b4a1e9c0fc03e3a0
Moleus <fafufuburr@gmail.com>
2023-11-04 19:16:37 +0300

    feat(sql): add insertion test

--- prog/isbd-curs/src/test/kotlin/org/ifmo/isbdcurs/persistence/RepositoryTest.kt
+++ prog/isbd-curs/src/test/kotlin/org/ifmo/isbdcurs/persistence/RepositoryTest.kt
@@ -1,13 +1,15 @@
 package org.ifmo.isbdcurs.persistence
 
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import org.assertj.core.api.Assertions.assertThat
+import org.ifmo.isbdcurs.models.BodyType
 import org.ifmo.isbdcurs.models.Person
+import org.ifmo.isbdcurs.models.Vehicle
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-
 
 @SpringBootTest
 class RepositoryTests {
@@ -15,9 +17,27 @@ class RepositoryTests {
     @Autowired
     lateinit var repository: PersonRepository
 
+    @Autowired
+    lateinit var vehicleRepository: VehicleRepository
+
     @BeforeEach
     fun before() {
         repository.deleteAll()
+    }
+
+    @Test
+    fun `should insert vehicle`() {
+        val vehicle = Vehicle(
+            plateNumber = "П123УП78",
+            model = "Lada",
+            manufactureYear = Instant.parse("2020-01-01T00:00:00Z"),
+            length = 1.0,
+            width = 1.0,
+            height = 1.0,
+            loadCapacity = 1.0,
+            bodyType = BodyType.CLOSED,
+        )
+        vehicleRepository.save(vehicle);
     }
 
     @Test
