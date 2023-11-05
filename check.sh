a245ac5035f9fd786bc0f037c82aebcf1f1c4301
Moleus <fafufuburr@gmail.com>
2023-11-05 19:25:55 +0300

    feat(ci): add workflow to run checks on push

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
-psql -h 127.0.0.1 -p 15432 -U postgres -f drop.sql && \
-psql -h 127.0.0.1 -p 15432 -U postgres -f create.sql && \
-psql -h 127.0.0.1 -p 15432 -U postgres -f functions.sql && \
-psql -h 127.0.0.1 -p 15432 -U postgres -f triggers.sql && \
-psql -h 127.0.0.1 -p 15432 -U postgres -f insert.sql
+# need to set PGHOST, PGPORT, PGPASSWORD and PGUSER
+psql -f drop.sql && \
+psql -f create.sql && \
+psql -f functions.sql && \
+psql -f triggers.sql && \
+psql -f insert.sql
