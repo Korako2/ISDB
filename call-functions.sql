02eff62db1671fcc46f0262559c945dd3642ecd0
Pavel Soloviev <66182149+Moleus@users.noreply.github.com>
2023-11-08 22:02:20 +0300

    feat(sql): add example where all business functions are called  (#44)
    * feat(sql): add example where all business functions are called
    
    * fix(sql): add missing cascade on delete
    
    * feat(tests): add `call-functions.sql` to checks

---
+++ call-functions.sql
@@ -0,0 +1,78 @@
+-- demo:
+-- select * from person join customer on person.id = customer.person_id ORDER BY person_id DESC LIMIT 1;
+
+DO
+$do$
+DECLARE
+    cargo_length int := 1;
+    v_driver_id int;
+    v_vehicle_id int;
+    v_order_id int;
+    v_customer_id int;
+    v_vehicles int[];
+    ve_plate_number varchar;
+    ve_body_type varchar;
+BEGIN
+SELECT add_new_customer(
+  v_first_name => 'Ivanov',
+  v_last_name => 'Ivanov',
+  v_gender => 'M',
+  v_date_of_birth => '1970-01-01',
+  v_middle_name => 'Ivanovich'
+) INTO v_customer_id;
+
+RAISE NOTICE 'Added new customer with id: %', v_customer_id;
+
+DELETE FROM driver WHERE passport = '1234567890';
+SELECT add_driver(
+  v_first_name => 'Oleg',
+  v_last_name => 'Olegovhich',
+  v_middle_name => 'Olegov',
+  v_gender => 'M',
+  v_date_of_birth => '1990-01-01',
+  v_passport => '1234567890',
+  v_bank_card_number => '1234567890123456'
+) INTO v_driver_id;
+
+RAISE NOTICE 'Driver added with id: %', v_driver_id;
+
+PERFORM add_driver_info(v_driver_id,
+  v_daily_rate => 1000,
+  v_rate_per_km => 10,
+  v_issue_date => '2020-01-01',
+  v_expiration_date => '2040-01-01',
+  v_license_number => 1234567890,
+  v_fuel_cards => ARRAY['1234567890123456', '1234567890123457'],
+  v_fuel_station_names => ARRAY['Gazprom', 'Lukoil']
+);
+
+SELECT vehicle_id FROM find_suitable_vehicles(
+  v_length => cargo_length,
+  v_width => 1,
+  v_height => 1,
+  v_cargo_type => 'BULK',
+  v_weight => 100
+) ORDER BY random() LIMIT 1 INTO v_vehicle_id;
+
+IF v_vehicle_id IS NULL THEN
+  RAISE EXCEPTION 'No suitable vehicles found ;(';
+END IF;
+
+SELECT plate_number, body_type INTO ve_plate_number, ve_body_type FROM vehicle WHERE id = v_vehicle_id;
+RAISE NOTICE 'Suitable vehicle "%" of type "%" found: %', ve_plate_number, ve_body_type, v_vehicle_id;
+
+SELECT add_order(
+  var_customer_id => v_customer_id,
+  distance => 100,
+  var_vehicle_id => v_vehicle_id,
+  v_weight => 100,
+  v_width => 1,
+  v_height => 1,
+  v_length => cargo_length,
+  v_cargo_type => 'BULK'
+  ) INTO v_order_id;
+
+RAISE NOTICE 'New order_id: %', v_order_id;
+
+END
+$do$;
