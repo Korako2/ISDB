3068df6d3c878dbece786f8514e8bd05cacb0a9b
Moleus <fafufuburr@gmail.com>
2023-11-08 19:25:17 +0300

    feat(tests): add `call-functions.sql` to checks

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 # need to set PGHOST, PGPORT, PGPASSWORD and PGUSER
-cat drop.sql create.sql functions.sql triggers.sql insert.sql | \
+cat drop.sql create.sql functions.sql triggers.sql insert.sql call-functions.sql | \
   psql -1 -v ON_ERROR_STOP=1 -q -f -
