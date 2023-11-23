53cd64a1efe73b75f7799d4faa91da6212bc08cd
Moleus <fafufuburr@gmail.com>
2023-11-23 22:41:31 +0300

    feat(ci): add trigger checks

--- check.sh
+++ check.sh
@@ -1,3 +1,6 @@
 # need to set PGHOST, PGPORT, PGPASSWORD and PGUSER
 cat drop.sql create.sql functions.sql triggers.sql insert.sql call-functions.sql | \
   psql -1 -v ON_ERROR_STOP=1 -q -f -
+
+# check triggers
+cat trigger-inserts.sql | psql -1 -v ON_ERROR_STOP=1 -q -f -
