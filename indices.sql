14b4e9c2df814b6898e89c8864f9a4347b9b1ed8
Kirill Guryanov <93494061+Korako2@users.noreply.github.com>
2023-11-23 19:13:11 +0300

    feat: add indexes (#67)

--- indices.sql
+++ indices.sql
@@ -1,5 +1,15 @@
-CREATE INDEX vehicle_movement_history_location_idx ON vehicle_movement_history USING btree (longitude, latitude);
+CREATE INDEX vehicle_movement_history_inx ON vehicle_movement_history (date);
 
-CREATE INDEX driver_status_history_status_idx ON driver_status_history USING hash (status);
+CREATE INDEX order_vehicle_id_idx ON orders(vehicle_id);
 
-CREATE INDEX order_statuses_status_idx ON order_statuses USING hash (status);
+CREATE INDEX vehicle_size_idx ON vehicle(length, width);
+
+ALTER TABLE vehicle DROP CONSTRAINT vehicle_plate_number_key;
+DROP INDEX vehicle_plate_number_key;
+CREATE INDEX vehicle_plate_number_key ON vehicle USING HASH (plate_number);
+ALTER TABLE vehicle
+ADD CONSTRAINT vehicle_plate_number_key
+CHECK (
+   plate_number ~ '^[А-Я]{1}\d{3}[А-Я]{2}\d{2}$'
+   OR plate_number ~ '^[А-Я]{1}\d{3}[А-Я]{2}\d{3}$'
+);
