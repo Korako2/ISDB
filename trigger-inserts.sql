a7d5df08e2dbe1ebab41c88165ddf090ae76c7be
Pavel Soloviev <66182149+Moleus@users.noreply.github.com>
2023-11-24 12:29:24 +0300

    feat(sql): don't truncate tables & new trigger-inserts exception hand… (#68)
    * feat(sql): don't truncate tables & new trigger-inserts exception handling
    
    * feat(ci): add trigger checks

--- trigger-inserts.sql
+++ trigger-inserts.sql
@@ -1,59 +1,75 @@
--- check_speed function trigger should throw exception if speed is greater than 170km/h
-TRUNCATE TABLE vehicle CASCADE;
-ALTER SEQUENCE vehicle_id_seq RESTART WITH 1;
-INSERT INTO vehicle (id, plate_number, model, manufacture_year, length, width, height, load_capacity, body_type)
-VALUES (1, 'А123ЕК152', 'a', '2000-01-01', 1.0, 1.0, 0.0, 1000, 'OPEN');
-
-TRUNCATE TABLE vehicle_movement_history CASCADE;
-INSERT INTO vehicle_movement_history (vehicle_id, date, latitude, longitude, mileage)
-VALUES (1, '2023-01-01 00:00:00', 1.0, 1.0, 0),
-       (1, '2023-01-01 00:02:00', 1.0, 1.0, 171 * 2);
-
--- check_vehicle_type should throw exception if we try to add PALLETS to OPEN vehicle
--- or BULK/TIPPER to CLOSED vehicle
-TRUNCATE TABLE vehicle CASCADE;
-ALTER SEQUENCE vehicle_id_seq RESTART WITH 1;
-INSERT INTO vehicle (id, plate_number, model, manufacture_year, length, width, height, load_capacity, body_type)
-VALUES (1, 'А123ЕК152', 'a', '2000-01-01', 1.0, 1.0, 0.0, 1000, 'OPEN');
-
-TRUNCATE TABLE orders CASCADE;
-ALTER SEQUENCE orders_id_seq RESTART WITH 1;
-INSERT INTO orders (id, customer_id, distance, price, order_date, vehicle_id)
-VALUES (1, 1, 800, 30000, '2023-10-25', 1);
-
-TRUNCATE TABLE cargo CASCADE;
-INSERT INTO cargo (id, order_id, cargo_type, weight, length, width, height)
-VALUES (1, 1, 'PALLETIZED', 1000, 1, 1, 1);
-
-
+DO
+$$
+DECLARE
+    v_vehicle_id int;
+    v_vehicle_2_id int;
+    v_person_id int;
+    v_person_2_id int;
+    v_order_id int;
+    v_driver_id int;
+    v_driver_2_id int;
+BEGIN
 -- prepare
-TRUNCATE TABLE person CASCADE;
-ALTER SEQUENCE person_id_seq RESTART WITH 1;
-INSERT INTO person(id, first_name, last_name, middle_name, gender, date_of_birth)
-VALUES (1, 'a', 'a', 'a', 'M', '2000-01-01'),
-       (2, 'b', 'b', 'b', 'M', '2000-01-01');
+DELETE FROM vehicle WHERE plate_number = 'А123ЕК152' OR plate_number = 'А123ЕК153';
+INSERT INTO vehicle (plate_number, model, manufacture_year, length, width, height, load_capacity, body_type)
+VALUES ('А123ЕК152', 'a', '2000-01-01', 1.0, 1.0, 0.0, 1000, 'OPEN')
+RETURNING id INTO v_vehicle_id;
 
-TRUNCATE TABLE vehicle CASCADE;
-ALTER SEQUENCE vehicle_id_seq RESTART WITH 1;
-INSERT INTO vehicle (id, plate_number, model, manufacture_year, length, width, height, load_capacity, body_type)
-VALUES (1, 'А123ЕК152', 'a', '2000-01-01', 1.0, 1.0, 0.0, 1000, 'OPEN'),
-       (2, 'А123ЕК153', 'a', '2000-01-01', 1.0, 1.0, 0.0, 1000, 'OPEN');
+INSERT INTO vehicle (plate_number, model, manufacture_year, length, width, height, load_capacity, body_type)
+VALUES ('А123ЕК153', 'b', '2000-01-01', 1.0, 1.0, 0.0, 1000, 'OPEN')
+RETURNING id INTO v_vehicle_2_id;
 
-TRUNCATE TABLE driver CASCADE;
-ALTER SEQUENCE driver_id_seq RESTART WITH 1;
-INSERT INTO driver (id, person_id, passport, bank_card_number)
-VALUES (1, 1, '1234567890', '1234567890123456'),
-       (2, 2, '1234567891', '1234567890123457');
+INSERT INTO person(first_name, last_name, middle_name, gender, date_of_birth)
+VALUES ('a', 'a', 'a', 'M', '2000-01-01')
+RETURNING id INTO v_person_id;
 
+INSERT INTO person(first_name, last_name, middle_name, gender, date_of_birth)
+VALUES ('b', 'b', 'b', 'M', '2000-01-01')
+RETURNING id INTO v_person_2_id;
+
+DELETE FROM driver WHERE person_id = v_person_id OR person_id = v_person_2_id OR PASSPORT = '1234567890' OR PASSPORT = '1234567891';
+INSERT INTO driver (person_id, passport, bank_card_number)
+VALUES (v_person_id, '1234567890', '1234567890123456')
+RETURNING id INTO v_driver_id;
+
+INSERT INTO driver (person_id, passport, bank_card_number)
+VALUES (v_person_2_id, '1234567891', '1234567890123457')
+RETURNING id INTO v_driver_2_id;
+
+DELETE FROM vehicle_movement_history WHERE vehicle_id = v_vehicle_id;
+
+BEGIN
+-- check_speed function trigger should throw exception if speed is greater than 170km/h
+INSERT INTO vehicle_movement_history (vehicle_id, date, latitude, longitude, mileage)
+VALUES (v_vehicle_id, '2023-01-01 00:00:00', 1.0, 1.0, 0),
+       (v_vehicle_id, '2023-01-01 00:02:00', 1.0, 1.0, 171 * 2);
+RAISE EXCEPTION 'check_speed_trigger should be triggered';
+
+EXCEPTION WHEN sqlstate 'T22A0' THEN
+  RAISE NOTICE '%', SQLERRM;
+END;
+
+DELETE FROM vehicle_ownership WHERE vehicle_id = v_vehicle_id OR vehicle_id = v_vehicle_2_id;
+BEGIN
 -- check_multiple_ownership_overlap - 1 driver, 2 vehicles, time overlap
-TRUNCATE TABLE vehicle_ownership CASCADE;
 INSERT INTO vehicle_ownership (vehicle_id, driver_id, ownership_start_date, ownership_end_date)
-VALUES (1, 1, '2023-01-01', '2023-01-05'),
-       (2, 1, '2023-01-03', '2023-01-08');
+VALUES (v_vehicle_id, v_driver_id, '2023-01-01', '2023-01-05'),
+       (v_vehicle_2_id, v_driver_id, '2023-01-03', '2023-01-08');
+RAISE EXCEPTION 'check_multiple_ownership_overlap should be triggered';
 
+EXCEPTION WHEN sqlstate 'T22A0' THEN
+  RAISE NOTICE '%', SQLERRM;
+END;
 
+BEGIN
 -- check_single_ownership_overlap - 2 drivers, one vehicle, time overlap
-TRUNCATE TABLE vehicle_ownership CASCADE;
 INSERT INTO vehicle_ownership (vehicle_id, driver_id, ownership_start_date, ownership_end_date)
-VALUES (1, 1, '2023-01-01', '2023-01-05'),
-       (1, 2, '2023-01-03', '2023-01-08');
+VALUES (v_vehicle_id, v_driver_id, '2023-01-01', '2023-01-05'),
+       (v_vehicle_id, v_driver_2_id, '2023-01-03', '2023-01-08');
+RAISE EXCEPTION 'check_multiple_ownership_overlap should be triggered';
+
+EXCEPTION WHEN sqlstate 'T22A0' THEN
+  RAISE NOTICE '%', SQLERRM;
+END;
+END;
+$$
