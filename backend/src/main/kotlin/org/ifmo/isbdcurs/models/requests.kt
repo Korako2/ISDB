d079e2cb254c6a5005df2b617b1325cf5c2c5c79
Kirill Guryanov <korako2@mail.ru>
2023-12-20 23:16:10 +0300

    refactor: rename method

--- backend/src/main/kotlin/org/ifmo/isbdcurs/models/requests.kt
+++ backend/src/main/kotlin/org/ifmo/isbdcurs/models/requests.kt
@@ -77,7 +77,7 @@ data class UserDto(
     var phone: String,
 )
 
-data class StorageAdressRequest(
+data class StorageAddressRequest(
     @NotEmpty(message = "Поле не может быть пустым")
     val country: String,
     @NotEmpty(message = "Поле не может быть пустым")
@@ -119,8 +119,8 @@ data class TimeParametersRequest(
 )
 
 data class OrderDataRequest(
-    val departureStoragePoint: StorageAdressRequest,
-    val deliveryStoragePoint: StorageAdressRequest,
+    val departureStoragePoint: StorageAddressRequest,
+    val deliveryStoragePoint: StorageAddressRequest,
     val orderParameters: PhysicalParametersRequest,
     val time: TimeParametersRequest
 )
