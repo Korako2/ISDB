4e2827bc296d2994a396cf35825517125f3fd499
Moleus <fafufuburr@gmail.com>
2023-12-21 18:14:22 +0300

    clean-up

--- backend/src/main/kotlin/org/ifmo/isbdcurs/util/StringUtils.kt
+++ backend/src/main/kotlin/org/ifmo/isbdcurs/util/StringUtils.kt
@@ -1,7 +1,5 @@
 package org.ifmo.isbdcurs.util
 
-import org.ifmo.isbdcurs.models.DriverStatus
-import org.ifmo.isbdcurs.models.OrderStatus
 import java.util.*
 
 val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
