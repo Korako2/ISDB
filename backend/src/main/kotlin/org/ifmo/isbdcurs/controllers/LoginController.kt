e961c17638d0d7df52bab86b11e01facf92d3dd7
Kirill Guryanov <korako2@mail.ru>
2023-12-28 23:51:41 +0300

    refactor(LoginController): delete unused code and apply code reformer

--- backend/src/main/kotlin/org/ifmo/isbdcurs/controllers/LoginController.kt
+++ backend/src/main/kotlin/org/ifmo/isbdcurs/controllers/LoginController.kt
@@ -1,7 +1,6 @@
 package org.ifmo.isbdcurs.controllers
 
 import jakarta.validation.Valid
-import kotlinx.datetime.LocalDate
 import org.ifmo.isbdcurs.models.AddCustomerRequest
 import org.ifmo.isbdcurs.models.AddNewCustomer
 import org.ifmo.isbdcurs.models.UserDto
@@ -14,9 +13,7 @@ import org.springframework.ui.ModelMap
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
-import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.util.*
 
 @Controller
 class LoginController(
@@ -28,12 +25,19 @@ class LoginController(
     @GetMapping("/register")
     fun showRegisterForm(model: Model): String {
         val date = SimpleDateFormat("yyyy-MM-dd").parse("2000-01-01")
-        model.addAttribute("addNewCustomer", AddNewCustomer("Happy", "VeryHappy", "F", date, "happy","password", "Happy@mail.ru", "88005553535", false))
+        model.addAttribute(
+            "addNewCustomer",
+            AddNewCustomer("Happy", "VeryHappy", "F", date, "happy", "password", "Happy@mail.ru", "88005553535", false)
+        )
         return "register"
     }
 
     @PostMapping("/register")
-    fun register(@ModelAttribute("addNewCustomer") @Valid addNewCustomer: AddNewCustomer, result: BindingResult, model: ModelMap): ModelAndView {
+    fun register(
+        @ModelAttribute("addNewCustomer") @Valid addNewCustomer: AddNewCustomer,
+        result: BindingResult,
+        model: ModelMap
+    ): ModelAndView {
         if (userService.isUniqueUserData(addNewCustomer, result) && !result.hasErrors()) {
             val userDto = UserDto(
                 username = addNewCustomer.username,
@@ -50,8 +54,6 @@ class LoginController(
     }
 
     private fun addCustomer(customer: AddNewCustomer): Long {
-        val sdf = SimpleDateFormat("yyyy-MM-dd")
-//        val date: Date = sdf.parse(customer.dateOfBirth)
         val date = customer.dateOfBirth!!
         val customerObj = AddCustomerRequest(
             firstName = customer.firstName,
